18f6b1c43d7b5f264b40d87f38ff303aa1818e52
Muhammad Khaidir <khaedir3018@gmail.com>
2025-06-28 11:09:49 +0800

    perbaikan judul cetak laporan

--- resources/views/unit-usaha/index.blade.php
+++ resources/views/unit-usaha/index.blade.php
@@ -23,7 +23,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">Cetak Laporan Prestasi</h5>
+                        <h5 class="modal-title">Cetak Laporan Unit Usaha</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
